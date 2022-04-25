c0685df5f7c1847b86c550d9ae21226fc0706667
Aimé Risson <aime.risson.1@gmail.com>
2022-04-25 14:45:06 +0200

    test

--- requests_ip_rotator/ip_rotator.py
+++ requests_ip_rotator/ip_rotator.py
@@ -30,8 +30,8 @@ ALL_REGIONS = EXTRA_REGIONS + [
 # Inherits from HTTPAdapter so that we can edit each request before sending
 class ApiGateway(rq.adapters.HTTPAdapter):
 
-    def __init__(self, site, regions=DEFAULT_REGIONS, access_key_id=None, access_key_secret=None, **kwargs):
-        super().__init__(**kwargs)
+    def __init__(self, site, regions=DEFAULT_REGIONS, access_key_id=None, access_key_secret=None):
+        super().__init__()
         # Set simple params from constructor
         if site.endswith("/"):
             self.site = site[:-1]
@@ -56,7 +56,8 @@ class ApiGateway(rq.adapters.HTTPAdapter):
         # Replace URL with our endpoint
         protocol, site = request.url.split("://", 1)
         site_path = site.split("/", 1)[1]
-        request.url = "https://" + endpoint + "/ProxyStage/" + site_path
+        #Set new request url
+        request.url = "https://" + endpoint + "/" + site_path
         # Replace host with endpoint host
         request.headers["Host"] = endpoint
         # Auto generate random X-Forwarded-For if doesn't exist.
@@ -75,7 +76,7 @@ class ApiGateway(rq.adapters.HTTPAdapter):
         # Init client
         session = boto3.session.Session()
         awsclient = session.client(
-            "apigateway",
+            "apigatewayv2",
             region_name=region,
             aws_access_key_id=self.access_key_id,
             aws_secret_access_key=self.access_key_secret
@@ -83,10 +84,9 @@ class ApiGateway(rq.adapters.HTTPAdapter):
         # If API gateway already exists for host, return pre-existing endpoint
         if not force:
             try:
-                current_apis = ApiGateway.get_gateways(awsclient)
+                current_apis = awsclient.get_apis()["Items"]
             except botocore.exceptions.ClientError as e:
                 if e.response["Error"]["Code"] == "UnrecognizedClientException":
-                    print(f"Could not create region (some regions require manual enabling): {region}")
                     return {
                         "success": False
                     }
@@ -94,162 +94,91 @@ class ApiGateway(rq.adapters.HTTPAdapter):
                     raise e
 
             for api in current_apis:
-                if "name" in api and self.api_name == api["name"]:
+                if self.api_name == api["Name"]:
                     return {
                         "success": True,
-                        "endpoint": f"{api['id']}.execute-api.{region}.amazonaws.com",
+                        "endpoint": f"{api['ApiId']}.execute-api.{region}.amazonaws.com",
                         "new": False
                     }
 
-        # Create simple rest API resource
-        create_api_response = awsclient.create_rest_api(
-            name=self.api_name,
-            endpointConfiguration={
-                "types": [
-                    "REGIONAL",
-                ]
-            }
+        # Create simple HTTP API resource
+        create_api_response = awsclient.create_api(
+            ProtocolType='HTTP',
+            Name=self.api_name
         )
+
+        http_api_id = create_api_response["ApiId"]
 
         # Get ID for new resource
-        get_resource_response = awsclient.get_resources(
-            restApiId=create_api_response["id"]
+        get_api_response = awsclient.get_api(
+            ApiId=http_api_id
         )
-        rest_api_id = create_api_response["id"]
-
-        # Create "Resource" (wildcard proxy path)
-        create_resource_response = awsclient.create_resource(
-            restApiId=create_api_response["id"],
-            parentId=get_resource_response["items"][0]["id"],
-            pathPart="{proxy+}"
-        )
-
-        # Allow all methods to new resource
-        awsclient.put_method(
-            restApiId=create_api_response["id"],
-            resourceId=get_resource_response["items"][0]["id"],
-            httpMethod="ANY",
-            authorizationType="NONE",
-            requestParameters={
-                "method.request.path.proxy": True,
-                "method.request.header.X-My-X-Forwarded-For": True
-            }
+        # Create "integration"
+        create_integration_response = awsclient.create_integration(
+            ApiId=http_api_id,
+            IntegrationType='HTTP_PROXY',
+            IntegrationMethod='ANY',
+            IntegrationUri=self.site + '/{proxy}',
+            PayloadFormatVersion='1.0'
         )
 
-        # Make new resource route traffic to new host
-        awsclient.put_integration(
-            restApiId=create_api_response["id"],
-            resourceId=get_resource_response["items"][0]["id"],
-            type="HTTP_PROXY",
-            httpMethod="ANY",
-            integrationHttpMethod="ANY",
-            uri=self.site,
-            connectionType="INTERNET",
-            requestParameters={
-                "integration.request.path.proxy": "method.request.path.proxy",
-                "integration.request.header.X-Forwarded-For": "method.request.header.X-My-X-Forwarded-For"
-            }
+        # Create "route" (wildcard proxy path)
+        create_resource_response = awsclient.create_route(
+            ApiId=http_api_id,
+            RouteKey='ANY /{proxy+}',
+            Target='integrations/' + create_integration_response['IntegrationId']
         )
 
-        awsclient.put_method(
-            restApiId=create_api_response["id"],
-            resourceId=create_resource_response["id"],
-            httpMethod="ANY",
-            authorizationType="NONE",
-            requestParameters={
-                "method.request.path.proxy": True,
-                "method.request.header.X-My-X-Forwarded-For": True
-            }
-        )
-
-        awsclient.put_integration(
-            restApiId=create_api_response["id"],
-            resourceId=create_resource_response["id"],
-            type="HTTP_PROXY",
-            httpMethod="ANY",
-            integrationHttpMethod="ANY",
-            uri=f"{self.site}/{{proxy}}",
-            connectionType="INTERNET",
-            requestParameters={
-                "integration.request.path.proxy": "method.request.path.proxy",
-                "integration.request.header.X-Forwarded-For": "method.request.header.X-My-X-Forwarded-For"
-            }
+        # Create "stage"
+        create_stage_response = awsclient.create_stage(
+            ApiId=http_api_id,
+            AutoDeploy=True,
+            Description='Default Stage',
+            StageName='$default'
         )
 
         # Creates deployment resource, so that our API to be callable
-        awsclient.create_deployment(
+        """ awsclient.create_deployment(
             restApiId=rest_api_id,
             stageName="ProxyStage"
-        )
+        ) """
 
         # Return endpoint name and whether it show it is newly created
         return {
             "success": True,
-            "endpoint": f"{rest_api_id}.execute-api.{region}.amazonaws.com",
+            "endpoint": f"{http_api_id}.execute-api.{region}.amazonaws.com",
             "new": True
         }
 
-    @staticmethod
-    def get_gateways(client):
-        gateways = []
-        position = None
-        complete = False
-        while not complete:
-            if isinstance(position, str):
-                gateways_response = client.get_rest_apis(
-                    limit=500,
-                    position=position
-                )
-            else:
-                gateways_response = client.get_rest_apis(
-                    limit=500
-                )
-
-            gateways.extend(gateways_response['items'])
-
-            position = gateways_response.get('position', None)
-            if position is None:
-                complete = True
-
-        return gateways
-
-    def delete_gateway(self, region, endpoints=None):
+    def delete_gateway(self, region):
         # Create client
         session = boto3.session.Session()
-        awsclient = session.client('apigateway',
-                                   region_name=region,
-                                   aws_access_key_id=self.access_key_id,
-                                   aws_secret_access_key=self.access_key_secret
-                                   )
-        # Extract endpoint IDs from given endpoints
-        endpoint_ids = []
-        if endpoints is not None:
-            for endpoint in endpoints:
-                endpoint_ids.append(endpoint.split(".")[0])
+        awsclient = session.client(
+            "apigatewayv2",
+            region_name=region,
+            aws_access_key_id=self.access_key_id,
+            aws_secret_access_key=self.access_key_secret
+        )
         # Get all gateway apis (or skip if we don't have permission)
         try:
-            apis = ApiGateway.get_gateways(awsclient)
+            apis = awsclient.get_apis()["Items"]
         except botocore.exceptions.ClientError as e:
             if e.response["Error"]["Code"] == "UnrecognizedClientException":
                 return 0
         # Delete APIs matching target name
         api_iter = 0
-        deleted = []
+        deleted = 0
         while api_iter < len(apis):
             api = apis[api_iter]
             # Check if hostname matches
-            if "name" in api and self.api_name == api["name"]:
+            if self.api_name == api["Name"]:
                 # Attempt delete
                 try:
-                    # If endpoints list is given, only delete if within list
-                    if endpoints is not None and api["id"] not in endpoint_ids:
-                        api_iter += 1
-                        continue
-                    success = awsclient.delete_rest_api(restApiId=api["id"])
+                    success = awsclient.delete_api(ApiId=api["ApiId"])
                     if success:
-                        deleted.append(api["id"])
+                        deleted += 1
                     else:
-                        print(f"Failed to delete API {api['id']}.")
+                        print(f"Failed to delete API {api['ApiId']}.")
                 except botocore.exceptions.ClientError as e:
                     # If timeout, retry
                     err_code = e.response["Error"]["Code"]
@@ -257,7 +186,7 @@ class ApiGateway(rq.adapters.HTTPAdapter):
                         sleep(1)
                         continue
                     else:
-                        print(f"Failed to delete API {api['id']}.")
+                        print(f"Failed to delete API {api['ApiId']}.")
             api_iter += 1
         return deleted
 
@@ -289,17 +218,17 @@ class ApiGateway(rq.adapters.HTTPAdapter):
         print(f"Using {len(self.endpoints)} endpoints with name '{self.api_name}' ({new_endpoints} new).")
         return self.endpoints
 
-    def shutdown(self, endpoints=None):
+    def shutdown(self):
         print(f"Deleting gateway{'s' if len(self.regions) > 1 else ''} for site '{self.site}'.")
         futures = []
+
         # Setup multithreading object
         with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
             # Send each region deletion to its own thread
             for region in self.regions:
-                futures.append(executor.submit(self.delete_gateway, region=region, endpoints=endpoints))
+                futures.append(executor.submit(self.delete_gateway, region=region))
             # Check outputs
-            deleted = []
+            deleted = 0
             for future in concurrent.futures.as_completed(futures):
                 deleted += future.result()
-        print(f"Deleted {len(deleted)} endpoints with for site '{self.site}'.")
-        return deleted
+        print(f"Deleted {deleted} endpoints with for site '{self.site}'.")
